733984903c49e27a8569605e04de42b1f4847b52
jaspervanriet <jaspervanriet@gmail.com>
2015-01-25 16:02:32 +0100

    Fixed crash on pre-Lollipop,  adjusted layout for lower resolution devices.

--- app/src/main/java/com/jaspervanriet/huntingthatproduct/Utils/Constants.java
+++ app/src/main/java/com/jaspervanriet/huntingthatproduct/Utils/Constants.java
@@ -21,9 +21,9 @@ public class Constants {
 	public static final String API_URL = "https://api.producthunt.com/v1/";
 	public static final String API_TOKEN_URL = "https://api.producthunt.com/v1/oauth/token";
 	public static final String CLIENT_ID =
-			"client_id";
+			"4edb602076ce9e58dda42e7e45f51bc6e2b096942edb9cba6ceb5cf794777842";
 	public static final String CLIENT_SECRET =
-			"client_secret";
+			"4a8400c890fccd0e05a4b7f4d59aa2d7c0e10af7f529d442d81c159e598e87ba";
 	public static final String GRANT_TYPE = "client_credentials";
 	public static String CLIENT_TOKEN = "";
 	public static long TOKEN_EXPIRES = 0;
